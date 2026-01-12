abcef8bbb73cd11e49e2de2858a4d30d60997024
Hammad Shaikh <hammadshaikh1890@gmail.com>
2026-01-13 02:46:01 +0500

    Time: 5 ms (83.36%) | Memory: 46.6 MB (95.38%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        mainStack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (mainStack.isEmpty()) return;
+
+        if (mainStack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+
+    public int top() {
+        if (mainStack.isEmpty()) return -1; // safety
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        if (minStack.isEmpty()) return -1; // safety
+        return minStack.peek();
+    }
+}
